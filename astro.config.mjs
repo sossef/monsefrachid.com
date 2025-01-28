14d019233914dcee7a0e89e6f04bbcdb55998c0d
Monsef Rachid <monsef.r@gmail.com>
2025-01-28 14:03:34 -0500

    Added XML sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  site: 'https://www.monsefrachid.com', 
+  integrations: [
+    tailwind(), 
+    alpinejs(), sitemap({
+      entryLimit: 10000, // Ensures only one sitemap file is generated
+    })
+  ]
 });
